32751c07e5e8f2572991b90cdccae0d8d78a4317
song8771 <song8771@gmail.com>
2025-07-18 14:51:45 +0900

    fix: db logic

--- database/db.js
+++ database/db.js
@@ -2,11 +2,14 @@ import { MongoClient } from 'mongodb';
 
 let db = null;
 
+const MONGODB_URI_LOCAL='mongodb://localhost:27017'
+const MONGODB_URI_ATLAS='mongodb+srv://song8771:1K6LOf5AW1bXCo4R@song-cluster.1tgghrr.mongodb.net/?retryWrites=true&w=majority&appName=song-cluster'
+
 export const connectDB = async () => {
     try {
         if (db)  return db; // 이미 연결되어 있다면 기존 db 반환
-        
-        const MONGODB_URI = process.env.MONGODB_URI;
+        console.log("🚀 ~ connectDB ~ process.env.NODE_ENV:", process.env.NODE_ENV)
+        const MONGODB_URI = process.env.NODE_ENV === 'development' ? MONGODB_URI_LOCAL : MONGODB_URI_ATLAS;
         const client = new MongoClient(MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
